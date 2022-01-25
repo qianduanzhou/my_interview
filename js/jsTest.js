190e139e51e3d5cb212657ac921ff74f94c2d3aa
周泓滨 <zhouhongbin@163.com>
2022-01-25 17:05:33 +0800

    feat: 更新

--- js/jsTest.js
+++ js/jsTest.js
@@ -16,7 +16,7 @@
 // console.log('script end')
 /**
  * 执行顺序 async await
- */ 
+ */
 //  async function async1(){
 //     console.log('async1 start');
 //      await async2();
@@ -25,7 +25,7 @@
 //  async function async2(){
 //      console.log('async2')
 //  }
- 
+
 //  console.log('script start');
 //  async1();
 //  console.log('script end')
@@ -50,9 +50,67 @@
 //     console.log('promise2');
 // });
 // console.log('script end');
-console.log(add())
-var num1 = 1,num2 = 2
-function add() {
-    num1 = 10,num2 = 20
-    return num1 + num2
+// console.log(add())
+// var num1 = 1,num2 = 2
+// function add() {
+//     num1 = 10,num2 = 20
+//     return num1 + num2
+// }
+
+let obj = {
+    a: 1,
+    o: {
+        b: {
+            c: 1
+        }
+    }
 }
+function defineReactive(obj) {
+    if(typeof obj === 'object') {
+        for(let key in obj) {
+            let value = obj[key]
+            defineReactive(value)
+            Object.defineProperty(obj, key, {
+                set(newVal) {
+                    if (newVal === value) return;
+                    console.log('set', key)
+                    typeof val === 'object' && defineReactive(newVal)
+                    return newVal
+                },
+                get() {
+                    console.log('get', key)
+                    return value
+                }
+            })
+        }
+    }
+}
+
+defineReactive(obj)
+console.log(JSON.stringify(obj.o))
+obj.o.b.c = 5
+obj.d = 5
+console.log(obj.d)
+
+// function reactive(obj) {
+//     let p = new Proxy(obj, {
+//         get(target, key, receiver) {
+//             if(Reflect.ownKeys(target).includes(key)) {
+//                 console.log('get', key)
+//                 const result = Reflect.get(target, key, receiver)
+//                 return typeof result === 'object' ? reactive(result) : result
+//             }
+//         },
+//         set(target, key, value, receiver) {
+//             console.log('set', key)
+//             Reflect.set(target, key, value, receiver)
+//         }
+//     })
+//     return p
+// }
+// let p = reactive(obj)
+// console.log(p.o.b)
+// console.log(p.o)
+// p.o.a = 5
+// p.d = 5
+// console.log(p.d)
