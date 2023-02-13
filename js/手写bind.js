e19ef6090a7d5b28b37ccf78c52b6e0d4de96c1d
周泓滨 <zhouhongbin@163.com>
2023-02-13 16:42:58 +0800

    docs: 更新

--- js/手写bind.js
+++ js/手写bind.js
@@ -12,7 +12,7 @@ Function.prototype.myBind = function (context, ...args) {
         // 第一种情况 :若是将 bind 绑定之后的函数当作构造函数，通过 new 操作符使用，则不绑定传入的 this，而是将 this 指向实例化出来的对象
         // 此时由于new操作符作用  this指向result实例对象  而result又继承自传入的_this 根据原型链知识可得出以下结论
         // this.__proto__ === result.prototype   //this instanceof result =>true
-        // this.__proto__.__proto__ === result.prototype.__proto__ === _this.prototype; //this instanceof _this =>true
+        // this.__proto__.__proto__ === result.prototype.__proto__ === _this.prototype; (result.prototype = Object.create(this.prototype);)//this instanceof _this =>true
         if (this instanceof _this === true) {
             // 此时this指向指向result的实例  这时候不需要改变this指向
             this[fn] = _this;
@@ -33,31 +33,33 @@ Function.prototype.myBind = function (context, ...args) {
 //用法如下
 
 function Person(name, age) {
+    this.name = name;
+    this.age = name;
     console.log(name); //'我是参数传进来的name'
     console.log(age); //'我是参数传进来的age'
     console.log(this); //构造函数this指向实例对象
 }
 // 构造函数原型的方法
 Person.prototype.say = function () {
-    console.log(123);
+    console.log(this.name);
 }
 let obj = {
-    objName: '我是obj传进来的name',
-    objAge: '我是obj传进来的age'
+    name: '我是obj传进来的name',
+    age: '我是obj传进来的age'
 }
 // 普通函数
 function normalFun(name, age) {
     console.log(name); //'我是参数传进来的name'
     console.log(age); //'我是参数传进来的age'
     console.log(this); //普通函数this指向绑定bind的第一个参数 也就是例子中的obj
-    console.log(this.objName); //'我是obj传进来的name'
-    console.log(this.objAge); //'我是obj传进来的age'
+    console.log(this.name); //'我是obj传进来的name'
+    console.log(this.age); //'我是obj传进来的age'
 }
 
 //先测试作为构造函数调用
 let bindFun = Person.myBind(obj, '我是参数传进来的name')
 let a = new bindFun('我是参数传进来的age')
-a.say() //123
+a.say() //我是obj传进来的name
 
 //再测试作为普通函数调用
 // let bindFun = normalFun.myBind(obj, '我是参数传进来的name')
